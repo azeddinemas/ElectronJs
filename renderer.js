1961c08504465e9f8e161e66b1c2baf06d0d8ef4
Zakaria KERKAZOU <Zakaria.KERKAZOU@dieze.center>
2023-06-06 10:45:14 +0100

    Fin du ma tache

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// renderer.js
+
+// This file runs in the renderer process
+
+// You can access Node.js APIs and Electron-specific APIs here
+
+// For example, you can use the 'ipcRenderer' module to communicate with the main process
+const { ipcRenderer } = require('electron')
+
+// Send a message to the main process
+ipcRenderer.send('message', 'Hello from renderer process!')
+
+// Receive a message from the main process
+ipcRenderer.on('reply', (event, arg) => {
+  console.log(arg) // Output: 'Hello from main process!'
+})
